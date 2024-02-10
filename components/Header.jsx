c100a069828a7c2de0f3c70a625257d067505af2
Gabriel Vincenzi <141547971+GabrielVincenzi@users.noreply.github.com>
2024-02-10 11:33:48 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,9 +8,6 @@ export const useDisableBodyScroll = (open) => {
     useEffect(() => {
         if (open) {
             document.body.style.overflow = 'hidden';
-        } else {
-            document.body.style.overflow = 'unset';
-        }
     }, [open]);
 };
 
