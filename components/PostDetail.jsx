b8bd4cbaa7f4afb6af5c88a8ec591bb10a4eeef6
Gabriel Vincenzi <141547971+GabrielVincenzi@users.noreply.github.com>
2024-02-18 09:38:07 +0100

    Update PostDetail.jsx

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -46,9 +46,9 @@ function PostDetail({ post }) {
 
     return (
         <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
-            <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
+            <div className='relative overflow-hidden shadow-md mb-6'>
                 <img src={post.featuredImage.url} alt={post.title}
-                    className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg' />
+                    className='object-top h-full w-full rounded-t-lg' />
             </div>
             <div className='px-8 lg:px-0'>
                 <div className='flex items-center mb-8 w-full'>
